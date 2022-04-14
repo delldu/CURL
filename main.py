d1ae4ff51f7c056e33a1699e4ff39c046b4d92dc
delldu <18588220928@163.com>
2022-04-15 01:54:15 +0800

    Study

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ np.set_printoptions(threshold=sys.maxsize)
 
 def main():
 
-    print("*** Before running this code ensure you keep the default batch size of 1. The code has not been engineered to support higher batch sizes. See README for more detail. Remove the exit() statement to use code. ***")
-    exit()
+    # print("*** Before running this code ensure you keep the default batch size of 1. The code has not been engineered to support higher batch sizes. See README for more detail. Remove the exit() statement to use code. ***")
+    # exit()
 
     writer = SummaryWriter()
 
